957e5bf8ce1eddfb330cfb9492a3d961071f98d5
Kyle Chivers <chiverskyle@gmail.com>
2023-03-19 11:00:16 -0700

    moved name to state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Yihua'
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi Kyle
+            Hi {this.state.name}
           </p>
           <button>
             Change Name
