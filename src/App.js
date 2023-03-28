22d8d4a2da552d8c59da61e7d1ed96bbb666ba6a
Kyle Chivers <chiverskyle@gmail.com>
2023-03-27 17:42:03 -0700

    adding api data to state

--- src/App.js
+++ src/App.js
@@ -14,9 +14,14 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => console.log(users))
-
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state);
+      }
+      ));
   }
 
   render() {
